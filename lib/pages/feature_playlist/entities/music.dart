f77618881847a79be05d4e6cc3813da13b3442e0
Rohmatul Laily <lailyjunior@gmail.com>
2022-03-23 22:16:51 +0700

    create unit test

--- lib/pages/feature_playlist/entities/music.dart
+++ lib/pages/feature_playlist/entities/music.dart
@@ -1,4 +1,38 @@
-class Music {
+import 'package:equatable/equatable.dart';
+
+class Music extends Equatable {
+  late final String wrapperType;
+  late final String kind;
+  late final int artistId;
+  late final int collectionId;
+  late final int trackId;
+  late final String artistName;
+  late final String collectionName;
+  late final String trackName;
+  late final String collectionCensoredName;
+  late final String trackCensoredName;
+  late final String artistViewUrl;
+  late final String collectionViewUrl;
+  late final String trackViewUrl;
+  late final String previewUrl;
+  late final String artworkUrl30;
+  late final String artworkUrl60;
+  late final String artworkUrl100;
+  late final double collectionPrice;
+  late final double trackPrice;
+  late final String releaseDate;
+  late final String collectionExplicitness;
+  late final String trackExplicitness;
+  late final int discCount;
+  late final int discNumber;
+  late final int trackCount;
+  late final int trackNumber;
+  late final int trackTimeMillis;
+  late final String country;
+  late final String currency;
+  late final String primaryGenreName;
+  late final bool isStreamable;
+
   Music({
     required this.wrapperType,
     required this.kind,
@@ -32,70 +66,39 @@ class Music {
     required this.primaryGenreName,
     required this.isStreamable,
   });
-  late final String wrapperType;
-  late final String kind;
-  late final int artistId;
-  late final int collectionId;
-  late final int trackId;
-  late final String artistName;
-  late final String collectionName;
-  late final String trackName;
-  late final String collectionCensoredName;
-  late final String trackCensoredName;
-  late final String artistViewUrl;
-  late final String collectionViewUrl;
-  late final String trackViewUrl;
-  late final String previewUrl;
-  late final String artworkUrl30;
-  late final String artworkUrl60;
-  late final String artworkUrl100;
-  late final double collectionPrice;
-  late final double trackPrice;
-  late final String releaseDate;
-  late final String collectionExplicitness;
-  late final String trackExplicitness;
-  late final int discCount;
-  late final int discNumber;
-  late final int trackCount;
-  late final int trackNumber;
-  late final int trackTimeMillis;
-  late final String country;
-  late final String currency;
-  late final String primaryGenreName;
-  late final bool isStreamable;
 
   Music.fromJson(Map<String, dynamic> json) {
-    wrapperType = json['wrapperType'];
-    kind = json['kind'];
-    artistId = json['artistId'];
-    collectionId = json['collectionId'];
+    wrapperType = json['wrapperType'] ?? "";
+    kind = json['kind'] ?? "";
+    artistId = json['artistId'] ?? "";
+    collectionId = json['collectionId'] ?? "";
     trackId = json['trackId'] ?? "";
-    artistName = json['artistName'];
-    collectionName = json['collectionName'];
-    trackName = json['trackName'];
-    collectionCensoredName = json['collectionCensoredName'];
-    trackCensoredName = json['trackCensoredName'];
-    artistViewUrl = json['artistViewUrl'];
-    collectionViewUrl = json['collectionViewUrl'];
-    trackViewUrl = json['trackViewUrl'];
-    previewUrl = json['previewUrl'];
-    artworkUrl30 = json['artworkUrl30'];
-    artworkUrl60 = json['artworkUrl60'];
-    artworkUrl100 = json['artworkUrl100'];
-    collectionPrice = json['collectionPrice'];
-    trackPrice = json['trackPrice'];
-    releaseDate = json['releaseDate'];
-    collectionExplicitness = json['collectionExplicitness'];
-    trackExplicitness = json['trackExplicitness'];
-    discCount = json['discCount'];
-    discNumber = json['discNumber'];
-    trackCount = json['trackCount'];
-    trackNumber = json['trackNumber'];
-    trackTimeMillis = json['trackTimeMillis'];
-    country = json['country'];
-    currency = json['currency'];
-    primaryGenreName = json['primaryGenreName'];
-    isStreamable = json['isStreamable'];
+    artistName = json['artistName'] ?? "";
+    collectionName = json['collectionName'] ?? "";
+    trackName = json['trackName'] ?? "";
+    collectionCensoredName = json['collectionCensoredName'] ?? "";
+    trackCensoredName = json['trackCensoredName'] ?? "";
+    artistViewUrl = json['artistViewUrl'] ?? "";
+    collectionViewUrl = json['collectionViewUrl'] ?? "";
+    trackViewUrl = json['trackViewUrl'] ?? "";
+    previewUrl = json['previewUrl'] ?? "";
+    artworkUrl30 = json['artworkUrl30'] ?? "";
+    artworkUrl60 = json['artworkUrl60'] ?? "";
+    artworkUrl100 = json['artworkUrl100'] ?? "";
+    collectionPrice = json['collectionPrice'] ?? 0.0;
+    trackPrice = json['trackPrice'] ?? 0.0;
+    releaseDate = json['releaseDate'] ?? "";
+    collectionExplicitness = json['collectionExplicitness'] ?? "";
+    trackExplicitness = json['trackExplicitness'] ?? "";
+    discCount = json['discCount'] ?? "";
+    discNumber = json['discNumber'] ?? "";
+    trackCount = json['trackCount'] ?? "";
+    trackNumber = json['trackNumber'] ?? "";
+    trackTimeMillis = json['trackTimeMillis'] ?? "";
+    country = json['country'] ?? "";
+    currency = json['currency'] ?? "";
+    primaryGenreName = json['primaryGenreName'] ?? "";
+    isStreamable = json['isStreamable'] ?? "";
   }
 
   Map<String, dynamic> toJson() {
@@ -133,4 +136,39 @@ class Music {
     _data['isStreamable'] = isStreamable;
     return _data;
   }
+
+  @override
+  List<Object> get props => [
+        wrapperType,
+        kind,
+        artistId,
+        collectionId,
+        trackId,
+        artistName,
+        collectionName,
+        trackName,
+        collectionCensoredName,
+        trackCensoredName,
+        artistViewUrl,
+        collectionViewUrl,
+        trackViewUrl,
+        previewUrl,
+        artworkUrl30,
+        artworkUrl60,
+        artworkUrl100,
+        collectionPrice,
+        trackPrice,
+        releaseDate,
+        collectionExplicitness,
+        trackExplicitness,
+        discCount,
+        discNumber,
+        trackCount,
+        trackNumber,
+        trackTimeMillis,
+        country,
+        currency,
+        primaryGenreName,
+        isStreamable,
+      ];
 }
